e58cdf763bf4f8dc30bb50ab5a8116e3bdecc4a0
Yamazaki Mitsuyoshi <Mitsuyoshi@Mitsuyoshis-MacBook-Air.local>
2012-07-26 22:05:39 +0900

    時間を管理するクラスを追加

---
+++ Timecard/TCTimeManager.h
@@ -0,0 +1,27 @@
+//
+//  TCTimeManager.h
+//  Timecard
+//
+//  Created by Mitsuyoshi Yamazaki on 7/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCTimeManager : NSObject {
+	NSMutableArray *_ranges;
+	NSTimeInterval _fromTime;
+}
+
++ (id)defaultManager;
++ (void)deleteManager;
+
+- (void)beginWorking;
+- (void)endWorking;
+- (void)beganFrom:(NSTimeInterval)interval;
+- (void)endFrom:(NSTimeInterval)interval;
+- (void)reset;
+
+- (NSArray *)timeRanges;
+
+@end
---
+++ Timecard/TCTimeManager.m
@@ -0,0 +1,145 @@
+//
+//  TCTimeManager.m
+//  Timecard
+//
+//  Created by Mitsuyoshi Yamazaki on 7/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TCTimeManager.h"
+
+@implementation TCTimeManager
+
+static id _instance = nil;
+static BOOL _willDelete = NO;
+
+#pragma mark - Singleton Methods
+
++ (id)defaultManager
+{
+	@synchronized(self) {
+		if (!_instance) {
+			_instance = [[self alloc] init];
+		}
+	}
+	return _instance;
+}
+
+- (id)init
+{
+	self = [super init];
+	
+	if (self) {
+		_ranges = [[NSMutableArray alloc] initWithCapacity:0];
+	}
+	return self;
+}
+
++ (id)allocWithZone:(NSZone *)zone
+{
+	@synchronized(self) {
+		if (!_instance) {
+			_instance = [super allocWithZone:zone];
+			
+			return _instance;
+		}
+	}
+	return nil;
+}
+
++ (void)deleteManager
+{
+	if (_instance) {
+		@synchronized(_instance) {
+			_willDelete = YES;
+			[_instance release];
+			_instance = nil;
+			_willDelete = NO;
+		}
+	}
+}
+
+- (id)copyWithZone:(NSZone *)zone
+{
+	return self;
+}
+
+- (id)retain
+{
+	return self;	
+}
+
+- (unsigned)retainCount
+{
+	return UINT_MAX;
+}
+
+- (oneway void)release
+{
+	@synchronized(self) {
+		if (_willDelete) {
+			[super release];
+		}
+	}
+}
+
+- (id)autorelease
+{
+	return self;
+}
+
+- (void)dealloc
+{	
+	[_ranges release], _ranges = nil;
+	
+	[super dealloc];
+}
+
+#pragma mark - Manage
+- (void)beginWorking {
+	[self beganFrom:0.0];
+}
+
+- (void)endWorking {
+	[self endFrom:0.0];
+}
+
+- (void)beganFrom:(NSTimeInterval)interval {
+	_fromTime = [[NSDate date] timeIntervalSince1970] - interval;
+}
+
+- (void)endFrom:(NSTimeInterval)interval {
+	
+	NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+	[dateFormatter setTimeStyle:NSDateFormatterNoStyle];
+	[dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+	
+	NSString *nowDateStr = [dateFormatter stringFromDate:[NSDate date]];
+	NSDate *nowDate = [dateFormatter dateFromString:nowDateStr];
+	NSLog(@"%@", nowDate);
+	[dateFormatter release];
+	
+	NSTimeInterval period = (([[NSDate date] timeIntervalSince1970] - interval) - _fromTime) / 60.0f;
+	if (period <= 0.0) {
+		return;
+	}
+	
+	NSTimeInterval beginTime = (_fromTime - [nowDate timeIntervalSince1970]) / 60.0f;
+	
+	NSRange range;
+	range.location = beginTime;
+	range.length = period;
+	
+	[_ranges addObject:[NSValue valueWithRange:range]];
+}
+
+- (void)reset {
+	[_ranges removeAllObjects];
+}
+
+#pragma mark - Result
+- (NSArray *)timeRanges {
+	return [NSArray arrayWithArray:_ranges];
+}
+
+@end
